0e87cb9371a9a3b5b6d0bc2bf49348d45ca02dde
Friedchicken-42 <paololego20+github@gmail.com>
2023-12-09 11:35:50 +0100

    day9

---
+++ bin/09/second.ml
@@ -0,0 +1,27 @@
+include Core
+
+let parse line = 
+    List.map int_of_string (String.split_on_char ' ' line)
+
+let rec difference = function
+| x :: y :: other -> (y - x) :: difference (y :: other)
+| _ -> []
+
+let all_zero = List.for_all (fun x -> x == 0)
+
+let rec calculate lst =
+    if all_zero lst then
+        0
+    else
+        let v = List.nth lst 0 in
+        let next = difference lst in
+
+        v - calculate next
+
+let () = 
+    let lines = read_lines () in
+    let values = List.map parse lines in
+    let outs = List.map calculate values in
+    let out = List.fold_left (+) 0 outs in
+    print_int out;
+    ()
